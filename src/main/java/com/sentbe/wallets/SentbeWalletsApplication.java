ef61724aa438b4eb3e6e4083972fc888b9d74c6d
cordock <5284173@naver.com>
2026-03-28 00:04:15 +0900

    Initial commit
    - 의존성 추가
    - DB 설정

---
+++ src/main/java/com/sentbe/wallets/SentbeWalletsApplication.java
@@ -0,0 +1,13 @@
+package com.sentbe.wallets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentbeWalletsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SentbeWalletsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sentbe/wallets/SentbeWalletsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sentbe.wallets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentbeWalletsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
