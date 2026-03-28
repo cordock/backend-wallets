0a8afe1197b3cca4f603c55d8c0e8367e495662d
cordock <5284173@naver.com>
2026-03-28 13:06:35 +0900

    chore: Spring Boot 버전 변경
    - 요구사항에 맞춰 안정성과 호환성을 고려하여 조정

--- src/main/java/com/sentbe/wallets/common/response/CustomResponseInterceptor.java
+++ src/main/java/com/sentbe/wallets/common/response/CustomResponseInterceptor.java
@@ -1,11 +1,11 @@
 package com.sentbe.wallets.common.response;
 
-import org.jspecify.annotations.Nullable;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
